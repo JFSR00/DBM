33c41c7c57073683ef647b4c81815995bc70ce56
JFSR00 <juanfsr00@gmail.com>
2021-04-15 10:15:02 +0200

    Práctica 6 problema 2 resuelto con éxito

--- DBM_P06_02/main.c
+++ DBM_P06_02/main.c
@@ -4,40 +4,57 @@
 int fi_media_floats(float *ptr_datos, unsigned int longitud_array, float *ptr_resultado);
 
 int main(){
-	double m1[4][4] =							// Matriz de ejemplo
-	{
-			{1.0,	2.0,	3.0,	4.0},
-			{5.0,	6.0,	7.0,	8.0},
-			{9.0,	10.0,	11.0,	12.0},
-			{13.0,	14.0,	15.0,	16.0}
-	};
+	float m1[17] __attribute__ ((aligned (32))) =	// Matriz de ejemplo
+	{ 1.0, 2.0, 3.0, 4.0, 5.0, 6.0, 7.0, 8.0, 9.0, 10.0, 11.0, 12.0, 13.0, 14.0, 15.0, 16.0, 17.0 };
 
-	double res[4][4];							// Matriz de resultado
+	float res __attribute__ ((aligned (32)));	// Variable del resultado
 
-	fi_sumar_matrices_4x4_double(m1, m1, res);	// Sumamos la matriz m1 consigo misma, es decir, se multiplica por 2 en este ejemplo
+	fi_media_floats(m1, 17, &res);				// Calculamos la media de la matriz de ejemplo, el resultado debe ser 9
 
-	for(int i=0; i<4; i++){						// Muestra en pantalla la matriz resultado
-		for(int j=0; j<4; j++){
-			printf("%.2f\t", res[i][j]);
-		}
-		printf("\n");
-	}
+	printf("%.2f\t", res);						// Muestra en pantalla el resultado de la operación
 
 	return 0;
 }
 
-int fi_sumar_matrices_4x4_double(const double *ptr_matriz_1, const double *ptr_matriz_2, double *ptr_matriz_suma){
-	if(ptr_matriz_1 == NULL || ptr_matriz_2 == NULL || ptr_matriz_suma == NULL){
-		return 0;								// Devolvemos 0 en caso de que algún puntero sea nulo
+int fi_media_floats(float *ptr_datos, unsigned int longitud_array, float *ptr_resultado){
+	if(ptr_datos == NULL || ptr_resultado == NULL || longitud_array == 0){
+		return 0;								// Devolvemos 0 en caso de que algún puntero sea nulo o la longitud 0
 	}else{
-		__m256d a, b;							// Creamos las variables que usan las funciones intrínsecas de AVX
+		__m256 inter, v;						// inter servirá para el cálculo de la media, v contendrá la suma de
+												// todos los elementos del array
+		__m128 resultado;						// res contendrá el resultado de la media
 
-		for(int i = 0; i<4; i++){				// Vamos recorriendo la matriz sumando de fila en fila
-			a = _mm256_load_pd(ptr_matriz_1 + (4 * i));
-			b = _mm256_load_pd(ptr_matriz_2 + (4 * i));
+		unsigned int n_bloques = longitud_array / 8;
+		unsigned int resto = longitud_array % 8;
 
-			_mm256_store_pd(ptr_matriz_suma + (4 * i), _mm256_add_pd(a, b));	// Realizamos la suma y almacenamos el resultado
+		float res;
+
+		v = _mm256_set1_ps(0.0);			// Ponemos a 0 la variable media
+
+		while(n_bloques > 0){					// Caso para más de 8 elementos
+			v = _mm256_add_ps(v, _mm256_load_ps(ptr_datos));
+			n_bloques--;
+			ptr_datos += 8;
 		}
+
+		v = _mm256_hadd_ps(v, v);
+		v = _mm256_hadd_ps(v, v);
+		//media = _mm256_hadd_ps(media, media);
+
+		inter = _mm256_permute2f128_ps(v, v, 1);
+		inter = _mm256_add_ps(inter, v);
+
+		resultado = _mm256_castps256_ps128(inter);
+
+		_mm_store_ss(&res, resultado);
+
+		while(resto > 0){						// Caso para menos de 8 elementos
+			res += *ptr_datos;
+			ptr_datos++;
+			resto--;
+		}
+
+		*ptr_resultado = res / longitud_array;
 
 		return 1;								// Devolvemos 1 al finalizar la función con éxito
 	}
