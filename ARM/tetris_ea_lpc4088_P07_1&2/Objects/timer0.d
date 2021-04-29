81ac92175cd5901fcbf902a82faf5cf7b2b879e3
Lab. DBM <juanfsr00@gmail.com>
2021-04-29 10:54:28 +0200

    Pruebas en placa prácticas 7 y 8, desarrollo de la práctica 8

--- ARM/tetris_ea_lpc4088_P07_1&2/Objects/timer0.d
+++ ARM/tetris_ea_lpc4088_P07_1&2/Objects/timer0.d
@@ -1,10 +1,9 @@
 .\objects\timer0.o: timer0.c
-.\objects\timer0.o: C:\Keil_v5\ARM\PACK\Keil\LPC4000_DFP\2.0.0\Device\Include\LPC407x_8x_177x_8x.h
-.\objects\timer0.o: C:\Keil_v5\ARM\PACK\ARM\CMSIS\5.0.0-Beta4\CMSIS\Include\core_cm4.h
+.\objects\timer0.o: C:\Keil_v5\ARM\PACK\Keil\LPC4000_DFP\1.1.0\Device\Include\LPC407x_8x_177x_8x.h
+.\objects\timer0.o: C:\Keil_v5\ARM\PACK\ARM\CMSIS\4.2.0\CMSIS\Include\core_cm4.h
 .\objects\timer0.o: C:\Keil_v5\ARM\ARMCC\Bin\..\include\stdint.h
-.\objects\timer0.o: C:\Keil_v5\ARM\PACK\ARM\CMSIS\5.0.0-Beta4\CMSIS\Include\core_cminstr.h
-.\objects\timer0.o: C:\Keil_v5\ARM\PACK\ARM\CMSIS\5.0.0-Beta4\CMSIS\Include\cmsis_armcc.h
-.\objects\timer0.o: C:\Keil_v5\ARM\PACK\ARM\CMSIS\5.0.0-Beta4\CMSIS\Include\core_cmfunc.h
-.\objects\timer0.o: C:\Keil_v5\ARM\PACK\ARM\CMSIS\5.0.0-Beta4\CMSIS\Include\core_cmsimd.h
-.\objects\timer0.o: C:\Keil_v5\ARM\PACK\Keil\LPC4000_DFP\2.0.0\Device\Include\system_LPC407x_8x_177x_8x.h
+.\objects\timer0.o: C:\Keil_v5\ARM\PACK\ARM\CMSIS\4.2.0\CMSIS\Include\core_cmInstr.h
+.\objects\timer0.o: C:\Keil_v5\ARM\PACK\ARM\CMSIS\4.2.0\CMSIS\Include\core_cmFunc.h
+.\objects\timer0.o: C:\Keil_v5\ARM\PACK\ARM\CMSIS\4.2.0\CMSIS\Include\core_cmSimd.h
+.\objects\timer0.o: C:\Keil_v5\ARM\PACK\Keil\LPC4000_DFP\1.1.0\Device\Include\system_LPC407x_8x_177x_8x.h
 .\objects\timer0.o: timer0.h
