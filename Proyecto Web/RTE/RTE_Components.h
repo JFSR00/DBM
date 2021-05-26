32007be3294cefc741175cb88c69f7fecd2d7039
Lab. DBM <juanfsr00@gmail.com>
2021-05-26 17:16:07 +0200

    Comienzo proyecto web

---
+++ Proyecto Web/RTE/RTE_Components.h
@@ -0,0 +1,40 @@
+
+/*
+ * Auto generated Run-Time-Environment Component Configuration File
+ *      *** Do not modify ! ***
+ *
+ * Project: 'web_project' 
+ * Target:  'Target 1' 
+ */
+
+#ifndef RTE_COMPONENTS_H
+#define RTE_COMPONENTS_H
+
+#define RTE_CMSIS_RTOS                  /* CMSIS-RTOS */
+        #define RTE_CMSIS_RTOS_RTX              /* CMSIS-RTOS Keil RTX */
+#define RTE_Drivers_ETH_KSZ8851SNL      /* Driver ETH KSZ8851SNL */
+#define RTE_Drivers_ETH_MAC0            /* Driver ETH_MAC0 */
+#define RTE_Drivers_PHY_DP83848C        /* Driver PHY DP83848C */
+#define RTE_Drivers_PHY_LAN8720         /* Driver PHY LAN8720 */
+#define RTE_Drivers_PHY_ST802RT1        /* Driver PHY ST802RT1 */
+#define RTE_Drivers_SPI0                /* Driver SPI0 */
+        #define RTE_Drivers_SPI1                /* Driver SPI1 */
+        #define RTE_Drivers_SPI2                /* Driver SPI2 */
+#define RTE_Drivers_USART0              /* Driver USART0 */
+        #define RTE_Drivers_USART1              /* Driver USART1 */
+        #define RTE_Drivers_USART2              /* Driver USART2 */
+        #define RTE_Drivers_USART3              /* Driver USART3 */
+        #define RTE_Drivers_USART4              /* Driver USART4 */
+#define RTE_Drivers_USART_PL2303        /* Driver USART PL2303 */
+#define RTE_Drivers_USBH0               /* Driver USBH0 */
+#define RTE_Network_Core                /* Network Core */
+          #define RTE_Network_Release             /* Network Release Version */
+#define RTE_Network_Interface_ETH_0     /* Network Interface ETH 0 */
+#define RTE_Network_Interface_PPP       /* Network Interface PPP */
+#define RTE_Network_Socket_TCP          /* Network Socket TCP */
+#define RTE_Network_Web_Server_RO       /* Network Web Server with Read-only Web Resources */
+#define RTE_USB_Core                    /* USB Core */
+#define RTE_USB_Host_0                  /* USB Host 0 */
+#define RTE_USB_Host_CustomClass        /* USB Host Custom Class */
+
+#endif /* RTE_COMPONENTS_H */
---
+++ Proyecto Web/main.cpp
@@ -0,0 +1,12 @@
+#include <LPC407x_8x_177x_8x.h>
+#include "RTE\Network\Net_Config_HTTP_Server.h"
+
+const uint32_t OscRateIn = 12000000;
+
+int main(void){
+	SystemInit();
+	
+	while (1){
+		
+	}
+}
